8d712074e19ed96a03af970e4f2dc9f54afdb00c
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-27 20:44:39 +0430

    created a route for greeting and also add the root route to the greeting index endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :greetings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    namespace :v1 do
+      resources :greetings
+    end
+  end
+  root "api/v1/greetings#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
