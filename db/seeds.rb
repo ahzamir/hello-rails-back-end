9032800779e51c965eb2c0956c38cd4043d3055a
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-27 20:35:29 +0430

    created the migratio and database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create some static data for the Greetings table
+
+Greeting.create(message: "Hello, World!")
+Greeting.create(message: "سلام دنیا!")
+Greeting.create(message: "こんにちは世界！")
+Greeting.create(message: "你好，世界！")
+Greeting.create(message: "Hallo Welt!")
+Greeting.create(message: "Привет, мир!")
+Greeting.create(message: "Bonjour le monde!")
+Greeting.create(message: "Hola, mundo!")
