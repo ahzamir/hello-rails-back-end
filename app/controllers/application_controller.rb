5def915c1de818a3b7f290bb2011dad652dd3c1e
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-27 20:59:47 +0430

    created the set_cors method for request header to have acesse for all methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    before_action :set_cors
+
+    def set_cors
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = '*'
+    end
 end
